610c59f6cc7f7838417c7a11e0666e5690e90167
juliiian99 <juliiianvilloria99@gmail.com>
2022-04-27 03:43:37 -0300

    fix loop

--- togit.js
+++ togit.js
@@ -33,26 +33,6 @@ client.on('ready', () => {
 
 client.on('message', async msg => {
 
-    if (msg.content.match('^°clear [0-9]+$')) {
-        msg.channel.bulkDelete(parseInt(msg.content.substr(msg.content.match(" ").index+1)) + 1);
-    }
-
-    if (msg.content.match('^°l .+$')) {
-        msg.member.voice.channel.join().then(connection =>{
-        connection.play(ytdl(msg.content.substr(msg.content.match(" ").index+1) , { filter: 'audioonly' }));
-        return; }).catch(console.error)
-    }
-
-    if (msg.content.match('^°leave$')) {
-        if (msg.member.voice.channel) {
-            if((await msg.member.voice.channel.join()).status == 0){
-                msg.member.voice.channel.leave();
-            }
-            temaikenes = []
-            cont = 0
-        }
-    }
-
     if (msg.content.match('^°clean$')) {
         temaikenes = []
         cont = 0
@@ -135,7 +115,6 @@ client.on('message', async msg => {
         }
     }
 
-
     if (msg.content.match('^°fs$')) {
         if(msg.guild.voice.connection.dispatcher != null){
             msg.guild.voice.connection.dispatcher.end()
@@ -143,11 +122,7 @@ client.on('message', async msg => {
     }
 
     if (msg.content.match('^°fs .+$')) {
-        cont = parseInt(msg.content.substr(msg.content.match(" ").index+1)) - 1
-        console.log(cont)
-        for(let j = 0; j < temaikenes.length; j++) {
-            console.log(j, temaikenes[j])
-        }
+        cont = parseInt(msg.content.substr(msg.content.match(" ").index+1)) - 2
         if(msg.guild.voice.connection.dispatcher != null){
             msg.guild.voice.connection.dispatcher.end()
         }
@@ -156,11 +131,12 @@ client.on('message', async msg => {
     if (msg.content.match('^°list$')) {
         const listado = new Discord.MessageEmbed()
         .setColor(0xff0000)
-        .setTitle("Temas en tu cola")
+        .setTitle("Temas en cola")
         .setDescription(
             await Promise.all(
                 temaikenes.map(async (temaiken, index) => {
                     if (typeof temaiken == typeof String()){
+                        console.log(cont, index,temaiken)
                         const filters1 = await ytsr.getFilters(temaiken)
                         const filter1 = filters1.get('Type').get('Video');
                         const result = await ytsr( filter1.url, {pages: 1 , gl:'ar', hl:'es' })
@@ -193,12 +169,7 @@ client.on('message', async msg => {
             .setDescription(`Duración: ${result.items[0].duration} \s
             Vistas: ${result.items[0].views}`)
             msg.channel.send(embed)
-            if(loop){
-                await again2()
-            }else{
-                cont++
-                await again()
-            }
+            again()
         }else{
             msg.reply('Ingrese a un canal de voz')
         }
@@ -208,43 +179,26 @@ client.on('message', async msg => {
         if(msg.member.voice.channel){
             const listNumber = parseInt(msg.content.substr(msg.content.match(" ").index+1)) - 1
             if(! isNaN(listNumber) ){
-                cont--
-                temaikenes.splice(listNumber-1, 1)
+                temaikenes.splice(listNumber, 1)
+                if(listNumber < cont){
+                    cont--
+                }
             }else{
-                msg.reply('Ingrese un número')
+                msg.reply('Ingrese un numero')
             }
         }else{
             msg.reply('Ingrese a un canal de voz')
         }
     }
 
-    async function again2(){
-        if(temaikenes.length != cont && temaikenes.length != 0 && temaikenes != undefined && temaikenes != null){
-            const filters1 = await ytsr.getFilters(temaikenes[cont])
-            const filter1 = filters1.get('Type').get('Video');
-            const result = await ytsr(filter1.url, {pages: 1, gl:'ar', hl:'es'})
-            conn = await msg.member.voice.channel.join()
-            if(conn.speaking.bitfield == 0 && result.items[0].url != undefined){
-                dispatcher = conn.play(ytdl(result.items[0].url), { filter: 'audioonly', volume: 0.5})
-                dispatcher.on('finish', () => {
-                    if(temaikenes.length == 0){
-                        timeout = setTimeout(() => { conn.disconnect() }, 5000 * 60) //Después de 5 min se desconecta
-                    }
-                    if(loop){
-                        cont++
-                        again2()
-                    }
-                })
-            }
-        }else{
-            cont -= temaikenes.length
-            again2()
-        }
-    }
-
     async function again(){
-        if(temaikenes.length !== 0){
-            const filters1 = await ytsr.getFilters(temaikenes[0])
+        if(temaikenes.length != cont && temaikenes.length != 0 && temaikenes != undefined && temaikenes != null){
+            let filters1 = null;
+            if(!loop){
+                filters1 = await ytsr.getFilters(temaikenes[0])
+            }else{
+                filters1 = await ytsr.getFilters(temaikenes[cont])
+            }
             const filter1 = filters1.get('Type').get('Video');
             const result = await ytsr(filter1.url, {pages: 1, gl:'ar', hl:'es'})
             conn = await msg.member.voice.channel.join()
@@ -257,8 +211,17 @@ client.on('message', async msg => {
                     if(!loop){
                         temaikenes.shift()
                         again()
+                    }else{
+                        console.log(cont, temaikenes)
+                        cont++
+                        again()
                     }
                 })
+            }
+        }else{
+            if(loop){
+                cont -= temaikenes.length
+                again()
             }
         }
     }
@@ -266,6 +229,5 @@ client.on('message', async msg => {
     if(dispatcher != null && timeout != null){
         dispatcher.on('start', () => {clearTimeout(timeout)})
     }
-
 
 })
